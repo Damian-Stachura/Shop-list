2184a0a73409a65c3ff0745b6d7112e273462002
Damian-Stachura <36454636+Damian-Stachura@users.noreply.github.com>
2018-05-11 20:06:10 +0200

    added some JS functionality, fixed couple of bugs

--- js/script.js
+++ js/script.js
@@ -9,26 +9,30 @@ $('.login--btn').on('click', function (event) {
         event.preventDefault();
         $('.login--panel__description').text('Witamy!').css('font-size', '2em');
         $('.login--panel').delay(900).animate({right: "100%"}, {queue: true});
-        $('.shopping--list--panel').delay(800).animate({left: "10%"}, {queue: true});
+        $('.shopping--list--panel').delay(800).animate({left: "0"}, {queue: true}).css({"display": "flex", "margin": "auto"});
         //if wrong credentials are given -> error
     } else {
         event.preventDefault();
         $('.login--panel__description').text('Wpisales bledny login lub haslo, sprobuj ponownie!')
             .css({'color': 'black',
-                'font-size': '1.1em'});
+                'font-size': '1.1em',
+                'text-shadow': 'none'});
+        $('#username').val("");
+        $('#password').val("");
     }
 })
 //function that resets panel description as well as the input values
 $('.reset--btn').on('click', function () {
     $('.login--panel__description').text('Podaj login i haslo, zeby sie zalogowac')
         .css({'color': 'white',
-            'font-size': '1.3em'});
+            'font-size': '1.3em',
+            'text-shadow': '1px 1px #000'});
 })
 
 /*SHOPPING PANEL FUNCTIONS*/
 
 //adding new item to the shop list
-$('.plus--btn').on('click', function() {
+var addNewShoppingItem = function() {
     //getting and setting values
     var newItemName = $('#newItem').val();
     var newItem = $('<li class="shopping--item">' + newItemName + '</li>');
@@ -48,4 +52,19 @@ $('.plus--btn').on('click', function() {
     }
     // reset the newItem input value
     $('#newItem').val("");
+}
+
+var plusBtn = $('.plus--btn');
+
+plusBtn.on('click', addNewShoppingItem);
+plusBtn.keypress(addNewShoppingItem);
+/*    plusBtn.on('keypress',function(event) {
+        if (event.keyCode === 13) {
+            addNewShoppingItem;
+        }
+    });*/
+
+//changing item's class onCLick
+$('.shopping--list').on('click', '.shopping--item', function() {
+    $(this).toggleClass('active');
 })
